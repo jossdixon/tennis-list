6f343e645ff1c2a8c0b497d5dd456ed1e08fb4c9
Joss Dixon <josscdixon@gmail.com>
2021-11-13 21:25:24 +0900

    views added

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,28 @@
 class BrandsController < ApplicationController
+  def index
+    @brands = Brand.all
+  end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+      redirect_to brands_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :image_url, :photo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'brands#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :brands, only: [ :index, :show, :new, :create ]
+  resources :brands, only: [ :index, :show, :new, :create ] do
+    resources :sponsorships, only: [ :new, :create ]
+  end
 
+  resources :sponsorships, only: [ :destroy ]
 end
